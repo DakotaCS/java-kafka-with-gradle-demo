fbef813676f781aece0289bd9e673299be4e091c
Dakota Soares <dakota.soares@execulink.com>
2024-03-13 11:40:41 -0400

    add producer demo

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,42 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger((ProducerDemo.class.getSimpleName()));
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        log.info("hello world log");
+
+        //Local Kafka Server
+        Properties props = new Properties();
+        //props.setProperty("bootstrap.servers","127.0.0.1:9092");
+
+        //Remote Kafka Server
+        props.put("bootstrap.servers", "https://distinct-thrush-9436-us1-kafka.upstash.io:9092");
+        props.put("sasl.mechanism", "SCRAM-SHA-256");
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ZGlzdGluY3QtdGhydXNoLTk0MzYkC4zJMwqs1oQvFmZhohOB4rY9nvvRXsN-Xak\" password=\"ZTY2M2I2MzItNWI1Zi00Yzc3LTliMDYtZmNjODlkYzlhNmYx\";");
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(props);
+
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java","hello world");
+
+    producer.send(producerRecord);
+    producer.close();
+
+
+
+
+    }
+}
