85a32a22a0bc02f83f8fda32e0a0ae977f96a99d
Dakota Soares <dakota.soares@execulink.com>
2024-04-10 13:58:32 -0400

    producer edits

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,43 @@
 package io.conduktor.demos.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class WikimediaChangesProducer {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws InterruptedException {
+
+        Properties props = new Properties();
+        //props.setProperty("bootstrap.servers","127.0.0.1:9092");
+
+        //Remote Kafka Server
+        props.put("bootstrap.servers", "https://distinct-thrush-9436-us1-kafka.upstash.io:9092");
+        props.put("sasl.mechanism", "SCRAM-SHA-256");
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ZGlzdGluY3QtdGhydXNoLTk0MzYkC4zJMwqs1oQvFmZhohOB4rY9nvvRXsN-Xak\" password=\"ZTY2M2I2MzItNWI1Zi00Yzc3LTliMDYtZmNjODlkYzlhNmYx\";");
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(props);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer,topic);
+
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+
+        EventSource eventSource = builder.build();
+
+        eventSource.start();
+
+        //produce for 10 minutes
+        TimeUnit.MINUTES.sleep(5);
+
     }
 }
+
