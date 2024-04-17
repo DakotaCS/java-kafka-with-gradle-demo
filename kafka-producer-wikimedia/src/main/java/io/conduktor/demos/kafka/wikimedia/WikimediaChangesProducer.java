941a8fcc0e0e73d0a89775d543000b39761f1065
Dakota Soares <dakota.soares@execulink.com>
2024-04-17 16:22:05 -0400

    add in optimizations

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -2,6 +2,7 @@ package io.conduktor.demos.kafka.wikimedia;
 
 import com.launchdarkly.eventsource.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.net.URI;
 import java.util.Properties;
@@ -20,6 +21,13 @@ public class WikimediaChangesProducer {
         props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ZGlzdGluY3QtdGhydXNoLTk0MzYkC4zJMwqs1oQvFmZhohOB4rY9nvvRXsN-Xak\" password=\"ZTY2M2I2MzItNWI1Zi00Yzc3LTliMDYtZmNjODlkYzlhNmYx\";");
         props.put("key.serializer", StringSerializer.class.getName());
         props.put("value.serializer", StringSerializer.class.getName());
+
+        props.put(ProducerConfig.LINGER_MS_CONFIG,"20");
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32 * 1024));
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy");
+
+
+
 
         KafkaProducer<String,String> producer = new KafkaProducer<String, String>(props);
 
